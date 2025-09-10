4454b4d9cfc2abf608a421bc3e9754d3944d6014
LZygmunt <lzygmunt0@gmail.com>
2025-09-10 21:01:06 +0200

    fix[web]: remove zooming when user enter in input on mobile

--- webApp/nuxt.config.ts
+++ webApp/nuxt.config.ts
@@ -19,6 +19,9 @@ export default defineNuxtConfig({
   },
   app: {
     baseURL: "/bill/",
+    head: {
+      viewport: "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0",
+    },
   },
 
   devServer: {
