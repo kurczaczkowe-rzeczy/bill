8b87e68036ca1e21624cad5c9dc67ca724f12c7b
LZygmunt <lzygmunt0@gmail.com>
2026-05-12 21:08:40 +0200

    build: add dev build version to build package

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,6 @@
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
 plugins {
     // this is necessary to avoid the plugins to be loaded multiple times
     // in each subproject's classloader
@@ -10,4 +13,23 @@ plugins {
     alias(libs.plugins.android.kotlin.multiplatform.library) apply false
     alias(libs.plugins.jetbrains.kotlin.jvm) apply false
     alias(libs.plugins.android.lint) apply false
+}
+
+val baseVersion = property("VERSION_NAME") as String
+val groupId = property("GROUP") as String
+
+val isDevBuild = gradle.startParameter.taskNames.any {
+    it.contains("Development", ignoreCase = true)
+}
+
+val timestamp = LocalDateTime.now()
+    .format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss"))
+
+allprojects {
+    group = groupId
+    version = if (isDevBuild) {
+        "$baseVersion-dev.$timestamp"
+    } else {
+        baseVersion
+    }
 }
